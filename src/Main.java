b6654591a0e1b552692bc3229477b348cad84c07
zack996 <hzcbaba@icloud.com>
2020-06-10 09:53:30 +0800

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Integer.bitCount(212429444));
+    }
+}
