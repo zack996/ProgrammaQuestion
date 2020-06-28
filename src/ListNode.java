29e641b6dbb3bc71c5e5852710daaa9368eed3bf
zack996 <hzcbaba@icloud.com>
2020-06-28 21:57:01 +0800

    增加遍历输出链表

--- src/ListNode.java
+++ src/ListNode.java
@@ -4,4 +4,11 @@ public class ListNode {
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public static void out(ListNode head){
+        while(head!=null){
+            System.out.println(head.val);
+            head=head.next;
+        }
+    }
 }
