6db9e75c18a8fc3de7be05c07a577debc6e83ac8
zack996 <hzcbaba@icloud.com>
2020-06-28 21:56:47 +0800

    剑指 Offer 22. 链表中倒数第k个节点

---
+++ src/GetKthFromEnd.java
@@ -0,0 +1,35 @@
+
+public class GetKthFromEnd {
+    public static void main(String[] args){
+        ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
+        ListNode.out(getKthFromEnd(head,2));
+        ListNode.out(recursion(head,2));
+    }
+    //迭代
+    public static ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode tail = head;
+        for (int i = 0; i < k; i++) {
+            tail=tail.next;
+        }
+        while (tail!=null){
+            head=head.next;
+            tail=tail.next;
+        }
+
+        return head;
+    }
+    //递归
+    static int tail = 0;
+
+    public static ListNode recursion(ListNode head, int k){
+        if(head==null){
+            return null;
+        }
+        ListNode tmp=recursion(head.next,k);
+        tail++;
+        if(tail==k){
+            return head;
+        }
+        return tmp;
+    }
+}
